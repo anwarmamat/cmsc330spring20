2e24754221b21dc36f2b8baadf4e2c6b5238e43e
Vinnie Caprarola <vinnie@vcaprarola.me>
2020-03-15 17:11:26 -0400

    Compiled sets for 4.05.0

--- project3/ocaml_version.sh
+++ project3/ocaml_version.sh
@@ -3,6 +3,8 @@ if [ $OCAML_VERSION = '4.07.1' ] ; then
     export OCAMLPATH=dep4.07.1
 elif [ $OCAML_VERSION = '4.07.0' ] ; then
     export OCAMLPATH=dep
+elif [ $OCAML_VERSION = '4.05.0' ] ; then
+    export OCAMLPATH=dep4.05.0
 else
     echo 'You must have OCaml version 4.07.0 or 4.07.1 for this project.'
     echo $OCAML_VERSION ' is not valid.'
